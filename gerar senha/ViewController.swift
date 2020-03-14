335c217dc253b56a3bc02df0f9cbb3bbdacba400
Frederyk Antunnes <frederykantunnes@gmail.com>
2020-03-14 14:02:41 -0300

    Initial Commit

---
+++ gerar senha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gerar senha
+//
+//  Created by Frederyk Antunnes on 14/03/20.
+//  Copyright © 2020 Frederyk Antunnes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
